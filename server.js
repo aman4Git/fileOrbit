0b68af10d85a4149b49e7ae8a31d1feb742c2aa2
Aman Mishra <amanm4345@gmail.com>
2024-05-23 16:32:32 +0530

    FEAT: Rotes configurations.

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const connectDB = require('./config/database');
 //Database configuration
 connectDB();
 
+//Route configuration
+app.use('/api/files', require('./routes/files'));
+
 //Start the server
 app.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
