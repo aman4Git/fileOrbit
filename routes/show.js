3be2b7aed49ba7a8acddad697b390c11c9c77b8b
Aman Mishra <amanm4345@gmail.com>
2024-05-28 12:18:40 +0530

    FEAT: Added download page.

---
+++ routes/show.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const File = require('../models/file');
+
+router.get('/:uuid', async (req, res) => {
+    try
+    {
+        //Get file by uuid
+        const file = await File.findOne({uuid: req.params.uuid});
+        
+        //Check if file exists
+        if(!file){
+            return res.render('download', {error: 'Link has been expired.'});
+        }
+
+        //Return response
+        return res.render('download', {
+            uuid    : file.uuid,
+            fileName: file.filename,
+            fileSize: file.size,
+            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+        });
+        
+    } catch (error) {
+        
+        return res.render('download', {error: 'Something went wrong.'});
+    }
+
+});
+
+
+module.exports =router;
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/database');
+const path = require('path');
+
+app.use(express.static('public'));
 
 //Database configuration
 connectDB();
 
+//Set Template engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 //Route configuration
 app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
 
 //Start the server
 app.listen(PORT, () => {
